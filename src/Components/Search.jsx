765cc400620cbbd57b54c2de867ef345eed39d1e
SarahGuarneri <sarahguarneri97@icloud.com>
2023-11-05 17:20:23 +0100

    modifiche finali

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,54 +1,15 @@
 import { useEffect, useState } from "react"
-import { Button, Card, Col, Container, Form, ListGroup, Row } from "react-bootstrap"
+import { Card, Col, Container, Form, ListGroup, Row } from "react-bootstrap"
+import { Wind, Droplet, ClockHistory } from "react-bootstrap-icons"
 
 const Search = () => {
 
-    // const [lat, setLat] = useState([])
-    // const [long, setLong] = useState([])
     const [meteo, setMeteo] = useState({})
     const [city, setCity] = useState('')
     const [isLoading, setIsLoading] = useState(false)
+    const [image, setImage] = useState('')
 
     console.log(city)
-
-    // const getLatAndLon = () => {
-    //     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=10&appid=4f38068d7e7c5d27e830b68d19fbb064`)
-    //     .then((res) => {
-    //         if(res.ok){
-    //             return res.json()
-    //         } else {
-    //             throw new Error ('Errore nel recupero della città')
-    //         }
-    //     })
-    //     .then((data) => {
-    //         console.log('recupero dati', data[0])
-    //         console.log('recupero dati', data[0].lat)
-    //         console.log('recupero dati', data[0].lon)
-    //         setLat (data[0].lat)
-    //         setLong (data[0].lon)
-    //         getCityWeather()
-    //     })
-    //     .catch((err) => {console.log('errore', err)})    
-    // }
-
-    // const getCityWeather = () => {
-    //     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=4f38068d7e7c5d27e830b68d19fbb064`)
-    //     .then((res) => {
-    //         if(res.ok){
-    //             return res.json()
-    //         } else {
-    //             throw new Error ('errore nel recupero meteo json')
-    //         }
-    //     })
-    //     .then((meteo) => {
-    //         console.log(meteo)
-    //         setMeteo (meteo)
-    //     })
-    //     .catch((err) => console.log('errore nel recupero del meteo', err))
-    // }
-
-    // console.log(meteo)
-
 
     const getCityWeather = () => {
         setIsLoading(true)
@@ -63,21 +24,32 @@ const Search = () => {
         .then((data) => {
             console.log(data)
             setMeteo(data)
+            // getImage()
             setIsLoading(false)
         })
         .catch((err) => {console.log('errore nel recupero dati', err)})
-        // .finally(() => {
-            
-        // })
     }
 
-    // useEffect(() => {
-    //     getLatAndLon()
-    //     getCityWeather()
-    // }, [lat, long])
+    const getImage = () => {
+        fetch(`https://api.teleport.org/api/urban_areas/slug:${city}/images/`)
+        .then((res) => {
+            if (res.ok){
+                return res.json()
+            } else {
+                throw new Error ('Errore nel recupero img')
+                // throw new Error (`Errore nel recupero dell'immagine. Codice di stato: ${res.status}`) ???? perché non funziona?
+            }
+        })
+        .then((image) => {
+            console.log(image)
+            setImage(image)
+        })
+        .catch((err) => {console.log('errore reupero immagine', err)})
+    }
 
     useEffect(() => {
         getCityWeather()
+        getImage()
     },[city])
 
 
@@ -95,33 +67,53 @@ const Search = () => {
                                     setCity (e.target.value)
                                 }}/>
                             </Form.Group>
-                            {/* <Button variant="primary" type="submit">
-                                Check!
-                            </Button> */}
                         </Form>
 
                     </Col>
                 </Row>
                 <Row>
                     <Col className="d-flex justify-content-center" xs={12}>
-                    {isLoading ? (<p className="fw-300 fs-2">Inserisci una città!</p>) : 
-                        <Card className="w-75 radiant-bg rounded-pill shadow ">
+                    {isLoading ? (<p className="fw-300 fs-2">Enter a city!</p>) : 
+                        <Card className="w-75 radiant-bg shadow rounded-4">
+                            {/* {image.photos &&
+                            <Card.Img variant="top" src={image.photos[0].image.mobile} alt="city-img"/>}                           */}
+                            {/* <div>
+                                {image.photos &&
+                                // <img src={image.photos[0].image.mobile} alt="img-city" className="w-50"/>
+                                <div style={{backgroundImage: `url('${image.photos[0].image.mobile}')`, backgroundSize: 'cover',  backgroundPosition: 'center', backgroundRepeat: 'no-repeat', height: '15em'}}>
+                                </div>
+                                }
+                            </div> */}
                             <Card.Body className="d-flex flex-column align-items-center">
-                                <Card.Title className="fw-700 fs-3">{city}</Card.Title>
+                                <Card.Title className="d-flex align-items-center w-100 justify-content-around">
+                                <p className="fw-300 m-0">{city}</p>
+                                <p className="m-0">Now</p>
+                                </Card.Title>
                                     <Card.Text className="d-flex flex-column align-items-center">
-                                        <p>{meteo.weather && meteo.weather[0].description}</p>
-                                        <p>{meteo.main && meteo.main.temp} °C</p>
-                                        <p>Pressure: {meteo.main && meteo.main.pressure} | Humidity: {meteo.main && meteo.main.humidity} %</p>
+                                        <p className="m-0 pt-4 fs-1">{meteo.main && meteo.main.temp} °C</p>
+                                        <p className="m-0 pb-4">{meteo.weather && meteo.weather[0].description}</p>
+                                        {/* <p className="m-0">Pressure: {meteo.main && meteo.main.pressure} | Humidity: {meteo.main && meteo.main.humidity} %</p> */}
                                     </Card.Text>
-                                    <div className="d-flex align-items-center">
+                                    <div className="d-flex align-items-center justify-content-evenly w-100">
                                         <p className="m-0">Max: {meteo.main && meteo.main.temp_max} °C</p>
                                         {meteo.weather && 
                                         <img src={`https://openweathermap.org/img/wn/${meteo.weather[0].icon}.png`} alt="" className="m-0"/>}
                                         <p className="m-0">Min: {meteo.main && meteo.main.temp_min} °C</p>
                                     </div>
+                                    <div className="pt-4 d-flex justify-content-around w-100">
+                                        <div>
+                                            <p className="m-0">{meteo.wind && <><Wind /> {meteo.wind.speed} km/h</>}</p>
+                                            <p className="m-0 mt-1">{meteo.main && <><ClockHistory className="me-2"/>{meteo.main.pressure}</>}</p>
+                                            <p className="m-0 mt-1">{meteo.main && <><Droplet /> {meteo.main.humidity} %</>}</p>
+                                        </div>
+                                    </div>                                    
                                 </Card.Body>
                             </Card>
                         }
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>                                        
                     </Col>
                 </Row>
             </Container>
